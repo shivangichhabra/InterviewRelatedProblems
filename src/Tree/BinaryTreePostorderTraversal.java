2ddebe13d018c144711a9d6f39e48c4d53f15f8c
shivangichhabra <keepintouch.shivangi26@gmail.com>
2017-07-12 20:02:30 -0500

    postOrder traversal recursive/iterative

--- src/Tree/BinaryTreePostorderTraversal.java
+++ src/Tree/BinaryTreePostorderTraversal.java
@@ -1,7 +1,9 @@
 package Tree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Shivangi on 7/9/2017.
@@ -16,5 +18,35 @@ public class BinaryTreePostorderTraversal {
         result.addAll(recursiveTraversal(root.right));
         result.add(root.val);
         return result;
+    }
+
+    public List<Integer> iterativeTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null)
+            return list;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            TreeNode top = stack.peek();
+
+            if(top.left == null && top.right == null){
+                stack.pop();
+                list.add(top.val);
+            }
+
+            if(top.right != null){
+                stack.push(top.right);
+                top.right = null;
+            }
+
+
+            if(top.left != null){
+                stack.push(top.left);
+                top.left = null;
+            }
+        }
+        return list;
     }
 }
