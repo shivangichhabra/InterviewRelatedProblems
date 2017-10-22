b9f0370a3f7fa69db541c167dc4cf79ba90bab3e
shivangichhabra <keepintouch.shivangi26@gmail.com>
2017-10-22 15:27:52 -0500

    odd even linked list

---
+++ src/LinkedList/OddEvenLinkedList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+/**
+ * Created by Shivangi on 10/22/2017.
+ */
+public class OddEvenLinkedList {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+        }
+    public ListNode oddEvenList(ListNode head) {
+        if(head != null){
+            ListNode oddHead = head;
+            ListNode evenHead = head.next;
+            ListNode temp = evenHead;
+
+            while(evenHead != null && evenHead.next != null){
+                oddHead.next = oddHead.next.next;
+                evenHead.next = evenHead.next.next;
+                oddHead = oddHead.next;
+                evenHead = evenHead.next;
+            }
+            oddHead.next = temp;
+        }
+        return head;
+
+    }
+}
