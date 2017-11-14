571082fbff6887e842195bfcbd5b82f97c92484e
Shivangi <shivangi.chhabra@cerner.com>
2017-11-14 00:17:06 -0600

    added two new problems

---
+++ src/String/RemoveArbitrarySpace.java
@@ -0,0 +1,41 @@
+package String;
+
+import java.util.StringTokenizer;
+
+/**
+ * Created by FS047207 on 11/13/2017.
+ */
+public class RemoveArbitrarySpace {
+
+    public String removeSpace(String s){
+        char[] c = s.trim().toCharArray();
+        boolean spaceFound = false;
+        int i=0, j=0;
+        while(i<s.length()){
+            if(c[i] != ' '){
+                if(c[i] == '.' || c[i] == ',' ||c[i] == '?' || c[i] == '!' && j-1 >= 0 && c[j-1] == ' ')
+                    c[j-1] = c[i++];
+                else
+                    c[j++] = c[i++];
+
+                spaceFound = false;
+            } else if(c[i++] == ' '){
+                if(!spaceFound){
+                    c[j++] = ' ';
+                    spaceFound = true;
+                }
+            }
+        }
+
+        //to handle remaining chars
+        while(j<s.length()){
+           c[j++] = ' ';
+        }
+        return String.valueOf(c).trim();
+    }
+
+    public static void main(String args[]){
+        RemoveArbitrarySpace s = new RemoveArbitrarySpace();
+        System.out.println(s.removeSpace("I am  Shivangi . Who are ! you know."));
+    }
+}
